215d1646593e84710966d4cbf6d1cd887a7eeee0
Davyd McColl <davydm@gmail.com>
2015-01-20 20:55:13 +0200

    * initial commit of hello world application

---
+++ app.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Application
+{
+    public static class Program
+    {
+        public static int Main(string[] args)
+        {
+            Console.WriteLine("Hello World\n");
+            return 0;
+        }
+    }
+}
+
